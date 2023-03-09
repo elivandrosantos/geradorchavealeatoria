5997b2cbadf029f07d154b2790d926439962aaad
Elivandro Santos <elivandrocsantos@gmail.com>
2023-03-09 19:01:11 -0300

    Update

--- core/forms.py
+++ core/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 
 class PasswordForm(forms.Form):
-    tamanho = forms.IntegerField(label='Tamanho da senha', min_value=1, max_value=74)
+    tamanho = forms.IntegerField(label='Digite tamanho de 1 á 74:', min_value=1, max_value=74)
