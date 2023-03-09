5964ad746de56c5388f7895dbeee763b09e76b50
Elivandro Santos <elivandrocsantos@gmail.com>
2023-03-09 16:16:49 -0300

    Update

--- core/urls.py
+++ core/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 
 from .views import home, generate_password
 
-app_name = 'core'
-
 urlpatterns = [
     path('', generate_password, name='gerador'),
     path('gerador/', home, name='home'),
